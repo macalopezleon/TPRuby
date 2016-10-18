eeadb01b21d15326b5a1b827d9304e4d648df107
macalopezleon <macalopezleon@hotmail.com>
2016-10-18 00:19:33 -0300

    intento de mercadopago

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,31 +1,59 @@
 class BuysController < ApplicationController
   require 'mercadopago.rb'
-  require 'mercadopago-api.rb'
+  skip_before_action :verify_authenticity_token
+  client_id='8060680399806762'
+  client_secret='vY9wVUceuL3VHNqu3cEgpdCtTWBosIlC'
 
   def create
-    mp = MercadoPago.new('8060680399806762', 'vY9wVUceuL3VHNqu3cEgpdCtTWBosIlC')
-    preferenceData = {
+    $mp = MercadoPago.new('8060680399806762','vY9wVUceuL3VHNqu3cEgpdCtTWBosIlC')
+    $mp.sandbox_mode(true)
+
+
+    cantidad=params[:cant].to_i
+    $preferenceData = {
       :items =>
       [
-        :title=>"Multicolor kite",
-        :quantity=>params[:cant],
+        :title=>"Credits",
+        :quantity=>cantidad,
         :unit_price=>10.0,
         :category_id=>"art",
         :currency_id=>"ARS" # Available currencies at: https://api.mercadopago.com/currencies
       ]
 
     }
-    preference = mp.create_preference(preferenceData)
-puts params[:cant].to_i
-    cred=current_user.credit + params[:cant].to_i
-    buys_params={ :credit=>cred,
-      :idMP => preference,
+    $preference = $mp.create_preference($preferenceData)
+    puts $preference
+#    AT=`curl -s -X POST -H 'content-type: application/x-www-form-urlencoded' 'https://api.mercadopago.com/oauth/token' -d 'grant_type=client_credentials' -d 'client_id=8060680399806762' -d 'client_secret=vY9wVUceuL3VHNqu3cEgpdCtTWBosIlC' | grep -o '"access_token":"[^"]*"' | sed -n 's/.*"access_token":"\(.*\)"/\1/p'`
+#
+  #  curl -X POST \
+  #  -H "Content-Type: application/json" \
+#    "https://api.mercadopago.com/users/test_user?access_token=$AT" \
+  #  -d '{"site_id":"MLA"}'
+
+    paymentData = Hash[
+      "transaction_amount" => 100,
+      "description" => "Title of what you are paying for",
+      "payment_method_id" => "rapipago",
+      "payer" => Hash[
+        "email" => "test_user_37653740@testuser.com"
+      ]
+    ]
+    payment = $mp.post("/v1/payments", paymentData)
+
+    if payment["status"] == "200"
+      puts payment
+      puts "entro"
+    else
+      puts "nada"
+    end
+
+    buys_params={ :credit=>params[:cant],
+      :idMP => $preference["response"]["id"],
       :user_id => current_user.id}
       @buy = Buy.new(buys_params)
-@buy.save
       respond_to do |format|
         if @buy.save
-          format.html { redirect_to buys_new_path, notice: preference['response']['init_point']}
+          format.html { redirect_to buys_path, notice: 'Se ha realizado la compra existosamente' }
           format.json { render :show, status: :created, location: @buy }
         else
           format.html { render :new }
@@ -34,26 +62,14 @@ puts params[:cant].to_i
       end
     end
 
-    def show
-    end
-
-
+    puts $preferenceResult
 
     def index
       @buys = Buy.where(user_id: (current_user)).all
     end
 
-    def create2
-      buys_params={ :credit=> 2,
-        :user_id => current_user.id}
-        @buy = Buy.new(buys_params)
-        respond_to do |format|
-          if @buy.save
-            format.json { render :show, status: :created, location: @buy }
-          else
-            format.html { render :new }
-            format.json { render json: @buy.errors, status: :unprocessable_entity }
-          end
-        end
-      end
+    def show
     end
+
+
+  end
