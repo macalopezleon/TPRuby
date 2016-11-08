15921302345a3bb9f1040934f523785604246b10
Your Name <macarenalopezleon@gmail.com>
2016-11-08 19:31:26 -0300

    modificacion seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.create!([
   {admin: true, email: "admin@admin.com", password: "123456", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 4, current_sign_in_at: "2016-10-22 14:59:25", last_sign_in_at: "2016-10-22 00:41:38", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", credit: 0},
-  {admin: false, email: "user@user.com", password: "123456", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 4, current_sign_in_at: "2016-10-22 14:59:25", last_sign_in_at: "2016-10-22 00:41:38", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", credit: 0}
+  {admin: false, email: "user@user.com", password: "123456", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 4, current_sign_in_at: "2016-10-22 14:59:25", last_sign_in_at: "2016-10-22 00:41:38", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", credit: 2}
 ])
 Buy.create!([
   {credit: 1, idMP: "213389056-0d185689-350b-45e7-9be3-0a74dd2a1d6d", user_id: 1},
